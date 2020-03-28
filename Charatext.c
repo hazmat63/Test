b6afe4196082b5a09e5e9c3f24a9d548ddb9b9c7
hazmat63 <62695381+hazmat63@users.noreply.github.com>
2020-03-28 11:27:35 +0530

    Q2.c

--- Charatext.c
+++ Charatext.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<ctype.h>                                     
+#include<ctype.h>Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â  
 #include<stdlib.h>
 #include<string.h>
 
